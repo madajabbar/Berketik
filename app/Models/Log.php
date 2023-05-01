aab12726755655d2d8a93dd0aa62f74d88ee0256
madajabbar <madajabbar22@gmail.com>
2023-05-01 23:25:59 +0700

    add log to dashboard

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -9,4 +9,11 @@ class Log extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function access(){
+        return $this->belongsTo(Access::class);
+    }
 }
