ea0cc7cae3165222c5b2c656d1c1dbc345ded9ee
madajabbar <madajabbar22@gmail.com>
2023-05-02 19:07:10 +0700

    check qr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,8 +28,8 @@
                     <h1 class="modal-title fs-5" id="exampleModalLabel">QR Anda</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">x</button>
                 </div>
-                <div class="modal-body mx-auto h-100 bg-warning">
-                <div class="container my-auto bg-danger" style="margin-top: 30%">
+                <div class="modal-body mx-auto h-100">
+                <div class="container m-auto">
                     {!!$qr!!}
                 </div>
                 </div>
