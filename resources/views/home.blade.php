4a3af5ba179ee921b3d3b7343ffda517d65547b8
madajabbar <madajabbar22@gmail.com>
2023-05-02 18:59:31 +0700

    check qr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,35 +21,25 @@
     </section>
     <!-- Button trigger modal -->
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-fullscreen">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">QR Anda</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">x</button>
-                </div>
-                <div class="modal-body">
-                    <div class="container">
-                        <div class="row align-items-center">
-                            <div class="col">
-                              One of three columns
-                            </div>
-                            <div class="col">
-                              One of three columns
-                            </div>
-                            <div class="col">
-                              One of three columns
-                            </div>
-                          </div>
-                      </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+    <center>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-fullscreen">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">QR Anda</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">x</button>
+                    </div>
+                    <div class="modal-body">
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </center>
     <!-- End Section Body 1 -->
 
     <!-- Start Section Body 2 -->
