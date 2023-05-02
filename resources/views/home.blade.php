401a6a6fc0450925989f9779f73c715e8cf245e3
madajabbar <madajabbar22@gmail.com>
2023-05-02 19:03:43 +0700

    check qr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,50 +21,25 @@
     </section>
     <!-- Button trigger modal -->
     <!-- Modal -->
-    <center>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-fullscreen">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">QR Anda</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">x</button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="container-fluid">
-                          <div class="row">
-                            <div class="col-md-4">.col-md-4</div>
-                            <div class="col-md-4 ml-auto">.col-md-4 .ml-auto</div>
-                          </div>
-                          <div class="row">
-                            <div class="col-md-3 ml-auto">.col-md-3 .ml-auto</div>
-                            <div class="col-md-2 ml-auto">.col-md-2 .ml-auto</div>
-                          </div>
-                          <div class="row">
-                            <div class="col-md-6 ml-auto">.col-md-6 .ml-auto</div>
-                          </div>
-                          <div class="row">
-                            <div class="col-sm-9">
-                              Level 1: .col-sm-9
-                              <div class="row">
-                                <div class="col-8 col-sm-6">
-                                  Level 2: .col-8 .col-sm-6
-                                </div>
-                                <div class="col-4 col-sm-6">
-                                  Level 2: .col-4 .col-sm-6
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">QR Anda</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">x</button>
+                </div>
+                <div class="modal-body mx-auto">
+                <div class="container m-auto">
+                    {!!$qr!!}
+                </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
         </div>
-    </center>
+    </div>
     <!-- End Section Body 1 -->
 
     <!-- Start Section Body 2 -->
