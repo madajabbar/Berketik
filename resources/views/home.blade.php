ddb42fbd17260c481819e98736689efd940005ca
madajabbar <madajabbar22@gmail.com>
2023-05-02 19:23:54 +0700

    check qr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
     <!-- Button trigger modal -->
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-fullscreen">
+        <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="container-fluid">
